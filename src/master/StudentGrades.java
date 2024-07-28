1a11ce7f4164c14e4d1505507bbc414d4712db3a
Farrel Baswara <55417641+farrelbas@users.noreply.github.com>
2024-07-28 22:43:28 +0700

    Add files via upload

--- src/master/StudentGrades.java
+++ src/master/StudentGrades.java
@@ -26,6 +26,8 @@ public class StudentGrades extends javax.swing.JFrame {
     private JTextField quizField;
     private JTextField utsField;
     private JTextField uasField;
+    private JLabel nameLabel;
+    private JLabel nimLabel;
     private JLabel averageLabel;
     private JLabel gradeLabel;
     private JLabel statusLabel;
@@ -35,138 +37,156 @@ public class StudentGrades extends javax.swing.JFrame {
      */
     public StudentGrades() {
         setTitle("Student Grades");
-        setSize(400, 300);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        JLabel nameLabel = new JLabel("Nama:");
-        nameLabel.setBounds(20, 20, 100, 25);
-        add(nameLabel);
+        JLabel name = new JLabel("Nama:");
+        name.setBounds(20, 20, 100, 25);
+        add(name);
 
         nameField = new JTextField();
         nameField.setBounds(120, 20, 150, 25);
         add(nameField);
 
-        JLabel nimLabel = new JLabel("NIM:");
-        nimLabel.setBounds(20, 60, 100, 25);
-        add(nimLabel);
+        JLabel nim = new JLabel("NIM:");
+        nim.setBounds(20, 60, 100, 25);
+        add(nim);
 
         nimField = new JTextField();
         nimField.setBounds(120, 60, 150, 25);
         add(nimField);
 
-        JLabel taskLabel = new JLabel("Nilai Tugas:");
-        taskLabel.setBounds(20, 100, 100, 25);
-        add(taskLabel);
+        JLabel task = new JLabel("Nilai Tugas:");
+        task.setBounds(20, 100, 100, 25);
+        add(task);
 
         taskField = new JTextField();
         taskField.setBounds(120, 100, 150, 25);
         add(taskField);
 
-        JLabel quizLabel = new JLabel("Nilai Kuis:");
-        quizLabel.setBounds(20, 140, 100, 25);
-        add(quizLabel);
+        JLabel quiz = new JLabel("Nilai Kuis:");
+        quiz.setBounds(20, 140, 100, 25);
+        add(quiz);
 
         quizField = new JTextField();
         quizField.setBounds(120, 140, 150, 25);
         add(quizField);
 
-        JLabel utsLabel = new JLabel("Nilai UTS:");
-        utsLabel.setBounds(20, 180, 100, 25);
-        add(utsLabel);
+        JLabel uts = new JLabel("Nilai UTS:");
+        uts.setBounds(20, 180, 100, 25);
+        add(uts);
 
         utsField = new JTextField();
         utsField.setBounds(120, 180, 150, 25);
         add(utsField);
 
-        JLabel uasLabel = new JLabel("Nilai UAS:");
-        uasLabel.setBounds(20, 220, 100, 25);
-        add(uasLabel);
+        JLabel uas = new JLabel("Nilai UAS:");
+        uas.setBounds(20, 220, 100, 25);
+        add(uas);
 
         uasField = new JTextField();
         uasField.setBounds(120, 220, 150, 25);
         add(uasField);
 
-        JButton hitungButton = new JButton("HITUNG");
-        hitungButton.setBounds(300, 20, 100, 25);
-        add(hitungButton);
+        JLabel nameLabelTitle = new JLabel("Nama :");
+        nameLabelTitle.setBounds(300, 20, 100, 25);
+        add(nameLabelTitle);
 
-        JButton resetButton = new JButton("RESET");
-        resetButton.setBounds(300, 60, 100, 25);
-        add(resetButton);
+        nameLabel = new JLabel();
+        nameLabel.setBounds(350, 20, 100, 25);
+        add(nameLabel);
 
-        JButton simpanButton = new JButton("SIMPAN");
-        simpanButton.setBounds(300, 100, 100, 25);
-        add(simpanButton);
+        JLabel nameNIMTitle = new JLabel("NIM :");
+        nameNIMTitle.setBounds(300, 60, 100, 25);
+        add(nameNIMTitle);
 
-        JLabel averageLabelTitle = new JLabel("Rerata:");
-        averageLabelTitle.setBounds(300, 140, 100, 25);
+        nimLabel = new JLabel();
+        nimLabel.setBounds(350, 60, 100, 25);
+        add(nimLabel);
+
+        JLabel averageLabelTitle = new JLabel("Rata - rata :");
+        averageLabelTitle.setBounds(300, 100, 100, 25);
         add(averageLabelTitle);
 
         averageLabel = new JLabel();
-        averageLabel.setBounds(300, 160, 100, 25);
+        averageLabel.setBounds(380, 100, 100, 25);
         add(averageLabel);
 
-        JLabel gradeLabelTitle = new JLabel("Grade:");
-        gradeLabelTitle.setBounds(300, 180, 100, 25);
+        JLabel gradeLabelTitle = new JLabel("Grade :");
+        gradeLabelTitle.setBounds(300, 140, 100, 25);
         add(gradeLabelTitle);
 
         gradeLabel = new JLabel();
-        gradeLabel.setBounds(300, 200, 100, 25);
+        gradeLabel.setBounds(350, 140, 100, 25);
         add(gradeLabel);
 
-        JLabel statusLabelTitle = new JLabel("Keterangan:");
-        statusLabelTitle.setBounds(300, 220, 100, 25);
+        JLabel statusLabelTitle = new JLabel("Keterangan :");
+        statusLabelTitle.setBounds(300, 180, 100, 25);
         add(statusLabelTitle);
 
         statusLabel = new JLabel();
-        statusLabel.setBounds(300, 240, 100, 25);
+        statusLabel.setBounds(380, 180, 100, 25);
         add(statusLabel);
+
+        JButton hitungButton = new JButton("HITUNG");
+        hitungButton.setBounds(40, 280, 100, 25);
+        add(hitungButton);
+
+        JButton resetButton = new JButton("RESET");
+        resetButton.setBounds(160, 280, 100, 25);
+        add(resetButton);
+
+        JButton simpanButton = new JButton("SIMPAN");
+        simpanButton.setBounds(280, 280, 100, 25);
+        add(simpanButton);
 
         hitungButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                hitungRerataDanGrade();
+                countAverage();
             }
         });
 
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                resetFields();
+                reset();
             }
         });
 
         simpanButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                simpanData();
+                saveData();
             }
         });
     }
 
-    private void hitungRerataDanGrade() {
+    private void countAverage() {
         try {
+            String studentName = nameField.getText();
+            String studentNIM = nimField.getText();
             int nilaiTask = Integer.parseInt(taskField.getText());
             int nilaiQuiz = Integer.parseInt(quizField.getText());
             int nilaiUTS = Integer.parseInt(utsField.getText());
             int nilaiUAS = Integer.parseInt(uasField.getText());
 
-            float rerata = (nilaiTask + nilaiQuiz + nilaiUTS + nilaiUAS) / 4.0f;
-            averageLabel.setText(String.valueOf(rerata));
+            float average = (nilaiTask + nilaiQuiz + nilaiUTS + nilaiUAS) / 4.0f;
+            averageLabel.setText(String.valueOf(average));
 
             String grade;
             String keterangan;
-            if (rerata >= 85) {
+            if (average >= 85) {
                 grade = "A";
                 keterangan = "Dinyatakan Lulus";
-            } else if (rerata >= 70) {
+            } else if (average >= 70) {
                 grade = "B";
                 keterangan = "Dinyatakan Lulus";
-            } else if (rerata >= 55) {
+            } else if (average >= 55) {
                 grade = "C";
                 keterangan = "Dinyatakan Lulus";
-            } else if (rerata >= 40) {
+            } else if (average >= 40) {
                 grade = "D";
                 keterangan = "Dinyatakan Tidak Lulus";
             } else {
@@ -176,13 +196,15 @@ public class StudentGrades extends javax.swing.JFrame {
 
             gradeLabel.setText(grade);
             statusLabel.setText(keterangan);
+            nameLabel.setText(studentName);
+            nimLabel.setText(studentNIM);
 
         } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(this, "Input tidak valid. Pastikan semua nilai diisi dengan angka.");
         }
     }
 
-    private void resetFields() {
+    private void reset() {
         nameField.setText("");
         nimField.setText("");
         taskField.setText("");
@@ -192,9 +214,11 @@ public class StudentGrades extends javax.swing.JFrame {
         averageLabel.setText("");
         gradeLabel.setText("");
         statusLabel.setText("");
+        nameLabel.setText("");
+        nimLabel.setText("");
     }
 
-    private void simpanData() {
+    private void saveData() {
         try (Connection conn = connection.database.getConnection()) {
             String sql = "INSERT INTO tb_student (student_name, student_nim, student_task, student_quiz, student_uts, student_uas, grade, average_grade, status, student_inserted_at, student_last_updated_at, student_softdel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
